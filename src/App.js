c70c2a7a66be4ebf0e7b15fcf43e3333c5c60c64
james-fleming394 <james.fleming394@gmail.com>
2022-12-21 13:21:16 -0500

    Hotel page create

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/hotels' element={<List/>}/>
-        {/* <Route path='/hotels/:id' element={<Hotel/>}/>
-        <Route path='/login' element={<Login/>}/> */}
+        <Route path='/hotels/:id' element={<Hotel/>}/>
+        {/* <Route path='/login' element={<Login/>}/> */}
       </Routes>
   );
 }
