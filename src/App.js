dfc6a2ec0fc2e31fa372d7ae408dbaffaa6653db
james-fleming394 <james.fleming394@gmail.com>
2022-12-21 11:04:15 -0500

    App routes set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Hotel from './pages/Hotel';
+import List from './pages/List';
+import Login from './pages/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        {/* <Route path='/hotels' element={<List/>}/>
+        <Route path='/hotels/:id' element={<Hotel/>}/>
+        <Route path='/login' element={<Login/>}/> */}
+      </Routes>
   );
 }
 
