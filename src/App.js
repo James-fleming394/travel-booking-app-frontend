a90ae4591d83ea60b1d33a6b4b71758e0e8c5ca0
james-fleming394 <james.fleming394@gmail.com>
2022-12-21 13:35:07 -0500

    Login Page set up

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Login from './pages/Login';
 function App() {
   return (
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/hotels' element={<List/>}/>
-        <Route path='/hotels/:id' element={<Hotel/>}/>
-        {/* <Route path='/login' element={<Login/>}/> */}
+        <Route path='/' element={<Home />}/>
+        <Route path='/hotels' element={<List />}/>
+        <Route path='/hotels/:id' element={<Hotel />}/>
+        <Route path='/login' element={<Login />}/>
       </Routes>
   );
 }
